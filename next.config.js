5411a761a816b77ba70b584520d143acf16a93cd
Austin Howard <cnfishead@hotmail.com>
2023-12-08 09:22:26 -0500

    change to next.config

--- next.config.js
+++ next.config.js
@@ -3,11 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     API_URL: process.env.API_URL || "https://austinhowardapi.azurewebsites.net/api/v1",
-    ENV: 'production',
+    ENV: "production",
     // ENV: "development",
   },
+
   images: {
-    domains: ["austinhoward.dev", "res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
